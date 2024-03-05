d5ac5931afc9a2d355dff2d5a398fce589844d84
jerome serriere <jerome.serriere@posteo.de>
2024-03-05 15:52:43 +0100

    Add Goal Create and Update method

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,4 +1,5 @@
 class GoalsController < ApplicationController
+
   def show
     @goal = Goal.find(params[:id])
   end
@@ -10,12 +11,32 @@ class GoalsController < ApplicationController
   def create
     @goal = Goal.new(goal_params)
     @goal.user = current_user
-    @goal.save
+    # @goal.save
 
-    # if @goal.save
-    #   redirect_to ?????????_path, notice: "Your goal was successfully created."
-    # else
-    #   render :new, status: :unprocessable_entity
-    # end
+    if @goal.save
+      redirect_to root_path, notice: "Your goal was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
+
+  def update
+    if params[:active] = true
+      goals = Goal.where(active: true, user_id: current_user)
+      goals.each do |goal|
+        goal.active = false
+        goal.save
+      end
+    end
+
+    #update current goal
+  end
+
+  private
+
+  def goal_params
+    params.require(:goal).permit(:title)
+    # params.require(:goal).permit(:title, :description, :active, :start_time, :end_time, :emoji, :progress)
+  end
+
 end
