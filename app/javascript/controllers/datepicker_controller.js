76f294787fbd2aaa62a9423766bda4512366f91f
jerome serriere <jerome.serriere@posteo.de>
2024-03-07 14:06:15 +0100

    Add Method for Datepicker

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -1,9 +1,16 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 import flatpickr from "flatpickr";
 
 // Connects to data-controller="datepicker"
 export default class extends Controller {
+  static targets = ["date"]
+
   connect() {
-    flatpickr(this.element)
+    console.log(this.dateTarget);
+    // flatpickr(this.element)
+    flatpickr(this.dateTarget, {
+      mode: "range",
+      showMonths: 2
+    });
   }
 }
