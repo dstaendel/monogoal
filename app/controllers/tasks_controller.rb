ffb2ad1f8789adf5540b5223c1b70e7846ec353f
jerome serriere <jerome.serriere@posteo.de>
2024-03-08 16:17:47 +0100

    Remove percentage from strong params

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -40,7 +40,7 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:title, :done, :percentage)
+    params.require(:task).permit(:title, :done)
   end
 
   def set_goal
