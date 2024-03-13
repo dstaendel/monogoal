1ac2ef301632f48f65022707ae4159fa162e3f6c
Dominic Ständel <dominic.staendel@hotmail.de>
2024-03-13 15:29:44 +0100

    fixed a bug that the navbar button toggler was performed when you go back to the page and appears without clicking on the toggler button before

--- app/javascript/navbar_button_toggler.js
+++ app/javascript/navbar_button_toggler.js
@@ -1,18 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
-  document.querySelector(".navbar-top").style.display = "none";
+  // document.querySelector(".navbar-top").style.display = "none";
 
   document.addEventListener("click", function(event) {
     const isButton = event.target.closest(".button-add-functionality");
+    // console.log(event.target.closest(".button-add-functionality"));
     const navbarTop = document.querySelector(".navbar-top");
 
     if (isButton) {
-      if (navbarTop.style.display === 'none') {
-        navbarTop.style.display = "";
-      } else {
-        navbarTop.style.display = "none";
-      }
+      console.log(isButton);
+      console.log(navbarTop);
+      navbarTop.classList.remove("d-none");
     } else {
-      navbarTop.style.display = "none";
+      navbarTop.classList.add("d-none");
     }
   });
 });
